808e5d7a3087433b2ec42db536833d044105518d
DZHURYNSKA LIUDMYLA <hello.its.me.luda@gmail.com>
2025-01-31 15:43:43 +0200

    hw3

---
+++ hw3/src/hw3/Main.java
@@ -0,0 +1,9 @@
+package hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        char a = '"';
+        char b = '.';
+        System.out.println(a + "This is shop-online app" + a  + b);
+    }
+}
